7c84d766fd55f1e9e64a32e7e34a6cfdcb23b844
reinierv4 <reinierv4@hotmail.com>
2017-11-12 14:47:54 +0100

    small change route book

--- src/Book.js
+++ src/Book.js
@@ -8,66 +8,36 @@ const Book = (props) => {
 			    <div className="book-top">
 	                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.thumbnail})`}}></div>
 	                <div className="book-shelf-changer">
-	                <Route 
-          				path="/search"
-          				render= {() => (
-					        <select onChange = {(event) => props.onChangeShelf(props.book, event.target.value)}>
-						        <option 
-						        	value="none" 
-						        	disabled
-						        	selected>
-						        	Move to...
-						        </option>
-						        <option 
-						        	value="currentlyReading" 
-						        	selected={props.book.shelf==="currentlyReading"}>
-						        	Currently Reading
-						        </option>
-						        <option 
-						        	value="wantToRead" 
-						        	selected={props.book.shelf==="wantToRead"}>
-						        	Want to Read
-						        </option>
-						        <option 
-						        	value="read" 
-						        	selected={props.book.shelf==="read"}
-						        >
-						        	Read
-						        </option>
-						        <option value="none">None</option>
-					        </select>
-					    )}
-					/>
-					<Route 
-          				exact path="/"
-          				render= {() => (
-					        <select onChange = {(event) => props.onChangeShelf(props.book, event.target.value)}>
-						        <option 
-						        	value="none" 
-						        	disabled>
-						        	Move to...
-						        </option>
-						        <option 
-						        	value="currentlyReading" 
-						        	selected={props.book.shelf==="currentlyReading"}>
-						        	Currently Reading
-						        </option>
-						        <option 
-						        	value="wantToRead" 
-						        	selected={props.book.shelf==="wantToRead"}>
-						        	Want to Read
-						        </option>
-						        <option 
-						        	value="read" 
-						        	selected={props.book.shelf==="read"}
-						        >
-						        	Read
-						        </option>
-						        <option value="none">None</option>
-					        </select>
-					    )}
-					/>
-				    </div>
+		                <select onChange = {(event) => props.onChangeShelf(props.book, event.target.value)}>
+							<option 
+							    value="none" 
+							    disabled
+							    selected>
+							       	Move to...
+							</option>
+							<option 
+							    value="currentlyReading" 
+							    selected={props.book.shelf==="currentlyReading"}>
+							    Currently Reading
+							</option>
+							<option 
+							    value="wantToRead" 
+							    selected={props.book.shelf==="wantToRead"}>
+							    	Want to Read
+							</option>
+							<option 
+							    value="read" 
+							    selected={props.book.shelf==="read"}>
+							    Read
+							</option>
+							<Route 
+				          		exact path="/"
+				          		render= {() => (
+							        <option value="none">None</option>
+							    )}
+							/>
+						</select>
+					</div>
 				</div>
 			    <div className="book-title">{props.book.title}</div>
 			    <div className="book-authors">{props.book.authors}</div>
